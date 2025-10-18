0a82eef7f07f1c111e99fba2c694db3a683f7d02
kobi1232 <kobi1232@gmail.com>
2025-10-18 12:51:53 +0300

    Update proc_load_bronze.sql

--- scripts/proc_load_bronze.sql
+++ scripts/proc_load_bronze.sql
@@ -1,107 +1,142 @@
 /*
 ===============================================================================
-Stored Procedure: Load Bronze Layer (Source -> Bronze)
+Stored Procedure: Load Bronze Layer (CSV -> Bronze)
 ===============================================================================
 Script Purpose:
-    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    This stored procedure loads raw data into the 'bronze' schema from external CSV files. 
     It performs the following actions:
     - Truncates the bronze tables before loading data.
-    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+    - Uses the `BULK INSERT` command to load data from CSV files into bronze tables.
 
 Parameters:
     None. 
-	  This stored procedure does not accept any parameters or return any values.
+    This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+CREATE OR ALTER PROCEDURE bronze.load_bronze
+AS
 BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
-	BEGIN TRY
-			SET @batch_start_time = GETDATE();
-			PRINT '================================================';
-			PRINT 'Loading Bronze Layer';
-			PRINT '================================================';
+    DECLARE @batch_start DATETIME = GETDATE();
+    PRINT '================================================';
+    PRINT 'Starting Bronze Layer Load at: ' + CONVERT(NVARCHAR, @batch_start, 120);
+    PRINT '================================================';
 
-			PRINT '------------------------------------------------';
-			PRINT 'Loading CRM Tables';
-			PRINT '------------------------------------------------';
+    PRINT '------------------------------------------------';
+    PRINT 'Loading NTX Tables';
+    PRINT '------------------------------------------------';
 
-			SET @start_time = GETDATE();
-			PRINT '>> Truncating Table: bronze.crm_accounts_info';
-			TRUNCATE TABLE bronze.crm_accounts_info;
-			PRINT '>> Inserting Data Into: bronze.crm_accounts_info';
-			BULK INSERT bronze.crm_accounts_info
-			FROM 'C:\Users\kobi\SQL\data_sets\crm_accounts_info.csv'
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-			);
-			SET @end_time = GETDATE();
-			PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-			PRINT '>> -------------';
+    -- ntx_movies_info
+    DECLARE @table_start DATETIME = GETDATE();
+    TRUNCATE TABLE bronze.ntx_movies_info;
+    PRINT '>> Inserting Data Into: bronze.ntx_movies_info';
+    BULK INSERT bronze.ntx_movies_info
+    FROM 'C:\Users\kobi\SQL\data_sets\ntx_movies_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '0x0a',
+        TABLOCK,
+        KEEPNULLS,
+        MAXERRORS = 10
+    );
+    PRINT 'Rows Loaded: ' + CAST(@@ROWCOUNT AS NVARCHAR(10));
+    PRINT '>> Table Duration: ' + CAST(DATEDIFF(SECOND, @table_start, GETDATE()) AS NVARCHAR(10)) + ' seconds';
 
-			SET @start_time = GETDATE();
-			PRINT '>> Truncating Table: bronze.crm_products_info';
-			TRUNCATE TABLE bronze.crm_products_info;
-			PRINT '>> Inserting Data Into: bronze.crm_products_info';
-			BULK INSERT bronze.crm_products_info
-			FROM 'C:\Users\kobi\SQL\data_sets\crm_products_info.csv'
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-			);
-			SET @end_time = GETDATE();
-			PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-			PRINT '>> -------------';
+    -- ntx_recommendation_logs
+    SET @table_start = GETDATE();
+    TRUNCATE TABLE bronze.ntx_recommendation_logs;
+    PRINT '>> Inserting Data Into: bronze.ntx_recommendation_logs';
+    BULK INSERT bronze.ntx_recommendation_logs
+    FROM 'C:\Users\kobi\SQL\data_sets\ntx_recommendation_logs.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '0x0a',
+        TABLOCK,
+        KEEPNULLS,
+        MAXERRORS = 10
+    );
+    PRINT 'Rows Loaded: ' + CAST(@@ROWCOUNT AS NVARCHAR(10));
+    PRINT '>> Table Duration: ' + CAST(DATEDIFF(SECOND, @table_start, GETDATE()) AS NVARCHAR(10)) + ' seconds';
 
-			SET @start_time = GETDATE();
-			PRINT '>> Truncating Table: bronze.crm_sales_pipeline';
-			TRUNCATE TABLE bronze.crm_sales_pipeline;
-			PRINT '>> Inserting Data Into: bronze.crm_sales_pipeline';
-			BULK INSERT bronze.crm_sales_pipeline
-			FROM 'C:\Users\kobi\SQL\data_sets\crm_sales_pipeline.csv'
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-			);
-			SET @end_time = GETDATE();
-			PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-			PRINT '>> -------------';
+    -- ntx_reviews
+    SET @table_start = GETDATE();
+    TRUNCATE TABLE bronze.ntx_reviews;
+    PRINT '>> Inserting Data Into: bronze.ntx_reviews';
+    BULK INSERT bronze.ntx_reviews
+    FROM 'C:\Users\kobi\SQL\data_sets\ntx_reviews.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '0x0a',
+        TABLOCK,
+        KEEPNULLS,
+        MAXERRORS = 10
+    );
+    PRINT 'Rows Loaded: ' + CAST(@@ROWCOUNT AS NVARCHAR(10));
+    PRINT '>> Table Duration: ' + CAST(DATEDIFF(SECOND, @table_start, GETDATE()) AS NVARCHAR(10)) + ' seconds';
 
-			SET @start_time = GETDATE();
-			PRINT '>> Truncating Table: bronze.crm_sales_teams';
-			TRUNCATE TABLE bronze.crm_sales_teams;
-			PRINT '>> Inserting Data Into: bronze.crm_sales_teams';
-			BULK INSERT bronze.crm_sales_teams
-			FROM 'C:\Users\kobi\SQL\data_sets\crm_sales_teams.csv'
-			WITH (
-				FIRSTROW = 2,
-				FIELDTERMINATOR = ',',
-				TABLOCK
-			);
-			SET @end_time = GETDATE();
-			PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-			PRINT '>> -------------';
+    -- ntx_search_logs
+    SET @table_start = GETDATE();
+    TRUNCATE TABLE bronze.ntx_search_logs;
+    PRINT '>> Inserting Data Into: bronze.ntx_search_logs';
+    BULK INSERT bronze.ntx_search_logs
+    FROM 'C:\Users\kobi\SQL\data_sets\ntx_search_logs.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '0x0a',
+        TABLOCK,
+        KEEPNULLS,
+        MAXERRORS = 10
+    );
+    PRINT 'Rows Loaded: ' + CAST(@@ROWCOUNT AS NVARCHAR(10));
+    PRINT '>> Table Duration: ' + CAST(DATEDIFF(SECOND, @table_start, GETDATE()) AS NVARCHAR(10)) + ' seconds';
 
-		SET @batch_end_time = GETDATE();
-		PRINT '=========================================='
-		PRINT 'Loading Bronze Layer is Completed';
-        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
-		PRINT '=========================================='
-	END TRY
-	BEGIN CATCH
-		PRINT '=========================================='
-		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
-		PRINT 'Error Message' + ERROR_MESSAGE();
-		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
-		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
-		PRINT '=========================================='
-	END CATCH
-END
+    -- ntx_users_info
+    SET @table_start = GETDATE();
+    TRUNCATE TABLE bronze.ntx_users_info;
+    PRINT '>> Inserting Data Into: bronze.ntx_users_info';
+    BULK INSERT bronze.ntx_users_info
+    FROM 'C:\Users\kobi\SQL\data_sets\ntx_users_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '0x0a',
+        TABLOCK,
+        KEEPNULLS,
+        MAXERRORS = 10
+    );
+    PRINT 'Rows Loaded: ' + CAST(@@ROWCOUNT AS NVARCHAR(10));
+    PRINT '>> Table Duration: ' + CAST(DATEDIFF(SECOND, @table_start, GETDATE()) AS NVARCHAR(10)) + ' seconds';
+
+    -- ntx_watch_history
+    SET @table_start = GETDATE();
+    TRUNCATE TABLE bronze.ntx_watch_history;
+    PRINT '>> Inserting Data Into: bronze.ntx_watch_history';
+    BULK INSERT bronze.ntx_watch_history
+    FROM 'C:\Users\kobi\SQL\data_sets\ntx_watch_history.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '0x0a',
+        TABLOCK,
+        KEEPNULLS,
+        MAXERRORS = 10
+    );
+    PRINT 'Rows Loaded: ' + CAST(@@ROWCOUNT AS NVARCHAR(10));
+    PRINT '>> Table Duration: ' + CAST(DATEDIFF(SECOND, @table_start, GETDATE()) AS NVARCHAR(10)) + ' seconds';
+
+    DECLARE @batch_end DATETIME = GETDATE();
+    PRINT '================================================';
+    PRINT 'Bronze Layer Load Completed Successfully';
+    PRINT '   - Start Time: ' + CONVERT(NVARCHAR, @batch_start, 120);
+    PRINT '   - End Time: ' + CONVERT(NVARCHAR, @batch_end, 120);
+    PRINT '   - Total Duration: ' + CAST(DATEDIFF(SECOND, @batch_start, @batch_end) AS NVARCHAR(10)) + ' seconds';
+    PRINT '================================================';
+END;
+GO
